48e82ec95a1cf88bf754f293a94a6a723899c9d2
apfwnls14 <apfwnls14@naver.com>
2023-12-28 15:00:24 +0900

    #2.5 웹팩으로 빌드하기

--- 2. 끝말잇기/WordRelay.jsx
+++ 2. 끝말잇기/WordRelay.jsx
@@ -2,8 +2,12 @@ const React = require('react');
 const { Component } = React;
 
 class WordRelay extends React.Component {
-  state = {};
-  render() {}
+  state = {
+    text: 'Hello, webpack',
+  };
+  render() {
+    return <h1>{this.state.text}</h1>;
+  }
 }
 
 module.exports = WordRelay;
